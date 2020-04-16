86a426c31eecf2ad115cfdf0fd9074d648349ed3
Beatriz Januário <bibi_januario@hotmail.com>
2020-04-16 20:05:24 -0300

    adicionando validação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { errors } = require('celebrate')
 const routes = require('./routes')
 
 const app = express()
@@ -7,6 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json()) //para utilizar json nas rotas
 app.use(routes)
+app.use(errors())
 
 //Tipos de parâmetros:
 //Query Params (req.query) = parâmetros enviados na rota após "?"
